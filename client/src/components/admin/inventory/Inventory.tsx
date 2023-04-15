607b1ca06551c6bca29dbbeeba0775fb9e306611
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2023-04-15 18:11:20 -0400

    data retrive

--- client/src/components/admin/inventory/Inventory.tsx
+++ client/src/components/admin/inventory/Inventory.tsx
@@ -28,3 +28,34 @@ interface InventoryColumns {
   tax: number;
   category: string;
 }
+
+
+const Inventory = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [inventoryData, setInventoryData] = useState<InventoryColumns[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [forUpdate, setForUpdate] = useState<boolean>(false);
+  const [toUpdateData, setToUpdateData] = useState();
+
+  const { setShowNotification } = useNotification();
+  const navigate = useNavigate();
+  const { AppStoreData, setAppStoreData } = useAppStore();
+
+  const prepareData = (data: InventoryColumns[]) => {
+    const formattedData = data.reduce((accumulator: any, currentValue) => {
+      return [
+        ...accumulator,
+        {
+          id: currentValue.id,
+          productName: currentValue.productName,
+          description: currentValue.description,
+          price: currentValue.price,
+          discount: currentValue.discount,
+          isAvailable: currentValue.isAvailable,
+          tax: currentValue.tax,
+          category: currentValue.category,
+        },
+      ];
+    }, []);
+    return formattedData;
+  };
